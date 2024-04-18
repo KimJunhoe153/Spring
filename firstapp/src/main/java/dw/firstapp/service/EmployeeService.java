3acb9935e64a71bb83c9e30fd0f348bff0bd61dc
KimJunhoe <fish.gave.seaweed@gmail.com>
2024-04-18 16:26:40 +0900

    20240418

--- firstapp/src/main/java/dw/firstapp/service/EmployeeService.java
+++ firstapp/src/main/java/dw/firstapp/service/EmployeeService.java
@@ -5,6 +5,9 @@ import dw.firstapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
     // 의존성주입
@@ -15,5 +18,47 @@ public class EmployeeService {
         // repository code
         employeeRepository.save(employee);
         return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()) {
+            // 예외처리
+            return null;
+        } else {
+            return employee.get();
+
+        }
+    }
+
+    public Employee updateEmployeeById(long id, Employee employee) {
+        // ID로 해당 데이터 찾기
+        Optional<Employee> employee1 = employeeRepository.findById(id);
+        if (employee1.isPresent()) {
+            // 데이터 업데이트
+            employee1.get().setEmail(employee.getEmail());
+            employee1.get().setFirstName(employee.getFirstName());
+            employee1.get().setLastName(employee.getLastName());
+            // 실제로 DB에 저장하기
+            employeeRepository.save(employee1.get());
+
+            return employee1.get();
+        } else {
+            return null;
+        }
+    }
+
+    public Employee deleteEmployeeById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            employeeRepository.deleteById(id);
+          return employee.get();
+        } else {
+            return null;
+        }
     }
 }
