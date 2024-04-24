a72ef32fb10e6b80ff4f36e8d22a44e23de7c9b6
KimJunhoe <fish.gave.seaweed@gmail.com>
2024-04-24 09:21:37 +0900

    20240424

--- gameshop/src/main/java/dw/gameshop/service/GameShopService.java
+++ gameshop/src/main/java/dw/gameshop/service/GameShopService.java
@@ -5,12 +5,8 @@ import dw.gameshop.model.Game;
 import dw.gameshop.model.User;
 import dw.gameshop.repository.GameShopRepository;
 import dw.gameshop.repository.UserRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
--- wholesale_company/src/main/java/dw/service/CustomerService.java
+++ wholesale_company/src/main/java/dw/service/CustomerService.java
@@ -12,9 +12,9 @@ import java.util.List;
 @Transactional
 public class CustomerService {
     @Autowired
-    CustomerRepository customerRepository;
+    CustomerRepository customerRepository; // 고객 데이터에 접근하기 위한 레포지토리 인터페이스
 
-    public List<Customer> getAllCustomer() {
+    public List<Customer> getAllCustomer() { // 고객 전체 목록을 반환한다
         return customerRepository.findAll(); // 데이터베이스에서 고객 목록을 조회
     }
 
